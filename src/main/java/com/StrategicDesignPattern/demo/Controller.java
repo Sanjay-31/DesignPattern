db961ac5e7a858d0492b699c3b2086ebce5ccc51
Sanjay-31 <shakya.sanjay.31@gmail.com>
2022-02-14 02:09:44 +0530

    "Builder Design Updated"

--- src/main/java/com/StrategicDesignPattern/demo/Controller.java
+++ src/main/java/com/StrategicDesignPattern/demo/Controller.java
@@ -3,7 +3,6 @@ package com.StrategicDesignPattern.demo;
 import com.StrategicDesignPattern.demo.Strategy.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
@@ -12,16 +11,20 @@ import java.util.Map;
 @RestController
 public class Controller {
 
+    Map<Integer, DayInterface>map=new HashMap<>();
     @GetMapping("/day/{currentDay}")
     public String getDay(@PathVariable Integer currentDay)
     {
-        Map<Integer, DayInterface>map=new HashMap<>();
+        setUp();
+        Day day=new Day(map.get(currentDay));
+        return day.get();
+    }
+    public void setUp()
+    {
         map.put(1,new Monday());
-        map.put(2,new Tuesday());
+        map.put(2,new Tuseday());
         map.put(3,new Wednesday());
         map.put(4,new Thursday());
         map.put(5,new Friday());
-        Day day=new Day(map.get(currentDay));
-        return day.get();
     }
 }
--- src/main/java/com/StrategicDesignPattern/demo/Strategy/Tuesday.java
+++
@@ -1,9 +0,0 @@
-package com.StrategicDesignPattern.demo.Strategy;
-
-public class Tuesday implements DayInterface
-{
-    @Override
-    public String CurrentDay() {
-        return "Today is Tuesday";
-    }
-}
